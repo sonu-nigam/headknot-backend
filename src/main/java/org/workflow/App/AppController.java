dfc7c684aa9e770fe7bea669d9069289201fe26a
Sonu Nigam <sonunigam064@gmail.com>
2025-05-16 16:03:44 +0530

    new changes

--- src/main/java/org/workflow/App/AppController.java
+++ src/main/java/org/workflow/App/AppController.java
@@ -1,25 +1,15 @@
 package org.workflow.App;
 
-import java.util.List;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.workflow.Shared.Enum.RootMenuEnum;
-import org.workflow.Shared.Utils.RootMenuUtils;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/")
 public class AppController {
 
     @GetMapping
-    public String RootPage(Model model, Pageable pageable) {
-
-        List<RootMenuEnum> rootMenu = RootMenuUtils.getMenuItems();
-        model.addAttribute("navMenu", rootMenu);
-
+    public String RootPage() {
         return "common/underConstruction";
     }
 
---
+++ src/main/java/org/workflow/Config/DataInitializer.java
@@ -0,0 +1,38 @@
+package org.workflow.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.workflow.User.UserEntity;
+import org.workflow.User.UserRepository;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public DataInitializer(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        UserEntity adminUserExists = userRepository.findByEmail("admin@test.com");
+        if (adminUserExists != null) return;
+
+        String hashedPassword = passwordEncoder.encode("password");
+
+        UserEntity user = new UserEntity();
+        user.setFullName("Jane Doe");
+        user.setEmail("admin@test.com");
+        user.setPassword(hashedPassword);
+        userRepository.save(user);
+    }
+}
